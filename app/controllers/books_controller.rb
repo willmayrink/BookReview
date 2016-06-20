615948512f7f2185d63160005dc6648be51225ee
will.mayrink <will.mayrink@gmail.com>
2016-06-20 20:06:00 -0300

    Adicionadas funções de adicionar livro, exibir características do livro, exibir todos os livros e link para cada livro

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,37 @@
+class BooksController < ApplicationController
+	before_action :find_book, only: [:show, :edit, :update, :destroy]
+	def index
+		@books = Book.all.order("created_at DESC")
+	end
+
+	def show
+		@book = Book.find(params[:id])
+
+	end
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+
+		if @book.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :description, :author, :year)
+
+	end
+	def find_book
+		@book = Book.find(params[:id])
+	end
+
+
+
+end
+
