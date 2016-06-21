5cc25744894c46577e4b40e91780b3db454848ea
will.mayrink <will.mayrink@gmail.com>
2016-06-21 00:02:11 -0300

    Usuários implementados, sessão implementada, exibição de links restringidas, e elementos visuais atualizados

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,16 @@ class BooksController < ApplicationController
 		@books = Book.all.order("created_at DESC")
 	end
 
+	
 	def show
-		@book = Book.find(params[:id])
-
 	end
+
+
 	def new
 		@book = Book.new
 	end
+
+
 
 	def create
 		@book = Book.new(book_params)
@@ -21,6 +24,23 @@ class BooksController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def edit
+	end
+
+	def update
+		if @book.update(book_params)
+			redirect_to book_path(@book)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@book.destroy
+		redirect_to root_path
+	end
+
 
 	private
 	def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  devise_for :installs
   resources :books
   root 'books#index'
   # The priority is based upon order of creation: first created -> highest priority.
